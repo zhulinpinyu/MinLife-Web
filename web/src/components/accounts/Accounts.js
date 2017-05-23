5d2dc173a751d73c8e4838d1489c3ad6b3f086a8
zhulinpinyu <mulixiang199016@gmail.com>
2017-05-24 06:53:24 +0800

    添加，编辑，删除 这三个操作的按钮

--- web/src/components/accounts/Accounts.js
+++ web/src/components/accounts/Accounts.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react'
-import { Table } from 'antd'
+import { Table, Popconfirm, Button } from 'antd'
 import styles from './Accounts.css'
 
 class Accounts extends Component {
+  deleteHandler(id) {
+    this.props.dispatch({
+      type: 'accounts/remove',
+      payload: id
+    })
+  }
+
+  renderActions(text, { id }) {
+    return (
+      <span className={styles.action}>
+        <Popconfirm title="要删除吗，这个类别可能已经在使用了？" onConfirm={this.deleteHandler.bind(this, id)} okText="删除" cancelText="算了">
+          <a href="">删除</a>
+        </Popconfirm>
+        <a href="">编辑</a>
+      </span>
+    )
+  }
+
   render() {
     const { accounts } = this.props
     const dataSource = accounts.map((a, i) => ({ ...a, sequence: i + 1 }))
     return (
       <div className={styles.normal}>
+        <div className={styles.create}>
+          <Button type="primary" icon="plus">
+            添加账户
+          </Button>
+        </div>
         <Table
           dataSource={dataSource}
           rowKey={item => item.id}
@@ -31,6 +54,7 @@ class Accounts extends Component {
           <Table.Column
             title="余额"
             key="action"
+            render={this.renderActions.bind(this)}
           />
         </Table>
       </div>
