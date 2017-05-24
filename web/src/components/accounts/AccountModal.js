59d1bd0efc8ce9b7fa414ddef61d533321dd6c59
zhulinpinyu <mulixiang199016@gmail.com>
2017-05-24 12:40:50 +0800

    add Account Modal Component

---
+++ web/src/components/accounts/AccountModal.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react'
+import { Modal, Form, Input } from 'antd'
+
+class AccountModal extends Component {
+  state = {
+    visible: false
+  }
+
+  showModalHandler(e) {
+    if (e) e.preventDefault()
+    this.setState({ visible: true })
+  }
+
+  hideModalHandler() {
+    this.setState({ visible: false })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    this.props.form.vaidateFileds((err, values) => {
+      if (!err) {
+        this.props.dispatch({
+          type: 'accounts/create',
+          payload: values
+        })
+      }
+      this.hideModalHandler()
+    })
+  }
+
+  render() {
+    const formItemLayout = {
+      labelCol: { span: 6 },
+      wrapperCol: { span: 14 }
+    }
+    const {
+      children,
+      modalKey,
+      title: modalTitle,
+      form: { getFieldDecorator },
+      record: { title, balance }
+    } = this.props
+    return (
+      <span>
+        <span onClick={this.showModalHandler.bind(this)}>
+          {children}
+        </span>
+        <Modal
+          key={modalKey}
+          title={modalTitle}
+          visible={this.state.visible}
+          onOk={this.handleSubmit.bind(this)}
+          onCancel={this.hideModalHandler.bind(this)}
+        >
+          <Form layout="horizontal" onSubmit={this.handleSubmit.bind(this)}>
+            <Form.Item
+              {...formItemLayout}
+              label="账户"
+            >
+              {
+                getFieldDecorator('title', {
+                  initialValue: title
+                })(<Input />)
+              }
+            </Form.Item>
+            <Form.Item
+              {...formItemLayout}
+              label="余额"
+            >
+              {
+                getFieldDecorator('balance', {
+                  initialValue: balance
+                })(<Input type="number" />)
+              }
+            </Form.Item>
+          </Form>
+        </Modal>
+      </span>
+    )
+  }
+}
+
+export default Form.create()(AccountModal)
