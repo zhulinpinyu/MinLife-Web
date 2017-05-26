2b878f1af3012347951e5e0bcdcdf110b40864a5
zhulinpinyu <mulixiang199016@gmail.com>
2017-05-26 20:23:48 +0800

    添加 create patch remove 三个action

--- web/src/models/bills.js
+++ web/src/models/bills.js
@@ -23,6 +23,18 @@ export default {
       yield put({ type: 'categories/fetch' })
       yield put({ type: 'accounts/fetch' })
       yield put({ type: 'members/fetch' })
+    },
+    *create({ payload: values }, { call, put }) {
+      yield call(billsService.create, values)
+      yield put({ type: 'fetch' })
+    },
+    *patch({ payload: { id, values } }, { call, put }) {
+      yield call(billsService.patch, { id, values })
+      yield put({ type: 'fetch' })
+    },
+    *remove({ payload: id }, { call, put }) {
+      yield call(billsService.remove, id)
+      yield put({ type: 'fetch' })
     }
   },
   subscriptions: {
