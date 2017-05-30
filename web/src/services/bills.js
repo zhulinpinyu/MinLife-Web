dcf711dbadccddc3120bb7bb5e12baf8c05a9127
zhulinpinyu <mulixiang199016@gmail.com>
2017-05-30 16:27:35 +0800

    账户不变，修改账单金额时 正确计算账户余额

--- web/src/services/bills.js
+++ web/src/services/bills.js
@@ -8,13 +8,17 @@ export const fetch = () => {
   return request(BILL_URL)
 }
 
+export const fetchById = (id) => {
+  return request(`${BILL_URL}/${id}`)
+}
+
 export const create = (values) => {
   updateAccount(values)
   return Common.create(BILL_URL, values)
 }
 
 export const patch = ({ id, values }) => {
-  updateAccount(values)
+  rollBackAndUpdateAccount({ id, values })
   return Common.patch(`${BILL_URL}/${id}`, values)
 }
 
@@ -30,4 +34,18 @@ const updateAccount = async ({ account_id, money }) => {
       balance: debt ? (balance + money) : (balance - money)
     }
   })
+}
+
+const rollBackAndUpdateAccount = async ({ id: bill_id, values }) => {
+  const { data: { account_id, money } } = await fetchById(bill_id)
+  const { data: { debt, balance } } = await Accounts.fetchById(account_id)
+  // Rollback account balance
+  await Accounts.patch({
+    id: account_id,
+    values: {
+      balance: debt ? (balance - money) : (balance + money)
+    }
+  })
+  // Update Account Balance
+  updateAccount(values)
 }
