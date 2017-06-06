ade7f37a55b37806c8cde013622cf589fa2836dc
zhulinpinyu <mulixiang199016@gmail.com>
2017-06-06 15:12:20 +0800

    组件化收入(IncomeForm)表单

--- web/src/components/bills/IncomeForm.js
+++ web/src/components/bills/IncomeForm.js
@@ -1,89 +1,41 @@
 import React, { Component } from 'react'
-import { Modal, Form, Input, Radio, Select, DatePicker } from 'antd'
+import { Form, Input, Select, DatePicker, Button } from 'antd'
 import moment from 'moment'
 
-import PaymentForm from './PaymentForm'
-import IncomeForm from './IncomeForm'
+class IncomeForm extends Component {
 
-class BillModal extends Component {
-  state = {
-    visible: false,
-    type: '支出'
-  }
-
-  handleBillTypeChange = (e) => {
-    this.setState({
-      ...this.state,
-      type: e.target.value
-    })
-  }
-
-  showModalHandler(e) {
-    if (e) e.preventDefault()
-    this.setState({ visible: true })
-  }
-
-  hideModalHandler() {
-    this.setState({ visible: false })
-  }
-
-  handleSubmit(values) {
+  handleSubmit(e) {
+    e.preventDefault()
     // const { form, onOk } = this.props
     // form.validateFields((err, fieldsValue) => {
     //   if (err) return
     //   const values = {
     //     ...fieldsValue,
+    //     type: '收入',
     //     money: parseFloat(fieldsValue.money),
     //     account_id: parseInt(fieldsValue.account_id, 10),
     //     member_id: parseInt(fieldsValue.member_id, 10),
     //     bill_date: fieldsValue.bill_date.format('YYYY-MM-DD HH:mm:ss')
     //   }
     //   onOk(values)
-    //   this.hideModalHandler()
     // })
-    this.props.onOk(values)
-    this.hideModalHandler()
-    console.log(values)
   }
 
-  renderPaidForm() {
-    const { accounts, categories, members, record } = this.props
-    return (
-      <PaymentForm
-        accounts={accounts}
-        categories={categories}
-        members={members}
-        onOk={this.handleSubmit.bind(this)}
-        record={record}
-      />
-    )
-  }
-
-  renderIncomeForm() {
-    const { accounts, members, record } = this.props
-    return (
-      <IncomeForm
-        accounts={accounts}
-        members={members}
-        onOk={this.handleSubmit.bind(this)}
-        record={record}
-      />
-    )
-  }
-
-  renderTransferForm() {
+  render() {
     const formItemLayout = {
       labelCol: { span: 6 },
       wrapperCol: { span: 14 }
     }
     const {
       accounts,
+      members,
       form: { getFieldDecorator },
       record: {
         money,
         currency,
         bill_date,
         account_id,
+        member_id,
         notes
       }
     } = this.props
@@ -99,7 +51,7 @@ class BillModal extends Component {
     )
 
     return (
-      <div>
+      <Form layout="horizontal" onSubmit={this.handleSubmit.bind(this)}>
         <Form.Item
           {...formItemLayout}
           label="金额"
@@ -123,7 +75,7 @@ class BillModal extends Component {
         </Form.Item>
         <Form.Item
           {...formItemLayout}
-          label="支出账户"
+          label="账户"
         >
           {
             getFieldDecorator('account_id', {
@@ -148,28 +100,29 @@ class BillModal extends Component {
         </Form.Item>
         <Form.Item
           {...formItemLayout}
-          label="转入账户"
+          label="成员"
         >
           {
-            getFieldDecorator('account_id', {
-              initialValue: account_id ? `${account_id}` : null,
+            getFieldDecorator('member_id', {
+              initialValue: member_id ? `${member_id}` : null,
               rules: [{
                 required: true,
-                message: '选择账户'
+                message: '选择成员'
               }]
             })(
               <Select>
                 {
-                  accounts.map((account) => {
+                  members.map((member) => {
                     return (
-                      <Select.Option key={account.id} value={`${account.id}`}>
-                        {account.title}
+                      <Select.Option key={member.id} value={`${member.id}`}>
+                        {member.title}
                       </Select.Option>
                     )
                   })
                 }
               </Select>
-          )}
+            )
+          }
         </Form.Item>
         <Form.Item
           {...formItemLayout}
@@ -195,70 +148,14 @@ class BillModal extends Component {
             })(<Input type="textarea" />)
           }
         </Form.Item>
-      </div>
-    )
-  }
-
-  renderForm() {
-    const { record: { type } } = this.props
-    const billType = type || this.state.type
-    // console.log(`record type: ${type}`)
-    // console.log(`state type: ${this.state.type}`)
-    // console.log(`billType: ${billType}`)
-    switch (billType) {
-      case '支出':
-        return this.renderPaidForm()
-      case '收入':
-        return this.renderIncomeForm()
-      case '转账':
-        return this.renderTransferForm()
-      default:
-        return this.renderPaidForm()
-    }
-  }
-
-  render() {
-    const {
-      children,
-      modalKey,
-      title: modalTitle,
-      form: { getFieldDecorator },
-      record: {
-        type
-      }
-    } = this.props
-
-    return (
-      <span>
-        <span onClick={this.showModalHandler.bind(this)}>
-          {children}
-        </span>
-        <Modal
-          key={modalKey}
-          title={modalTitle}
-          visible={this.state.visible}
-          maskClosable={false}
-          footer={null}
-          onCancel={this.hideModalHandler.bind(this)}
+        <Form.Item
+          wrapperCol={{ span: 12, offset: 6 }}
         >
-          <Form.Item
-            wrapperCol={{ offset: 6, span: 18 }}
-          >
-            {getFieldDecorator('type', {
-              initialValue: type || this.state.type
-            })(
-              <Radio.Group onChange={this.handleBillTypeChange}>
-                <Radio.Button value="支出">支出</Radio.Button>
-                <Radio.Button value="收入">收入</Radio.Button>
-                <Radio.Button value="转账">转账</Radio.Button>
-              </Radio.Group>
-            )}
-          </Form.Item>
-          {this.renderForm()}
-        </Modal>
-      </span>
+          <Button type="primary" htmlType="submit">Save</Button>
+        </Form.Item>
+      </Form>
     )
   }
 }
 
-export default Form.create()(BillModal)
+export default Form.create()(IncomeForm)
