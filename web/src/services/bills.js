a963b11fe6dc8658485e529ba4fdc69b2203f8e6
zhulinpinyu <mulixiang199016@gmail.com>
2017-06-07 18:21:12 +0800

    重构 收入 支出 转账 service

--- web/src/services/bills.js
+++ web/src/services/bills.js
@@ -30,12 +30,12 @@ export const create = async (values) => {
 }
 
 export const patch = async ({ id, values }) => {
-  await rollBackAndUpdateAccount({ id, values })
+  await rollbackAndUpdateAccount({ id, values })
   return Common.patch(`${BILL_URL}/${id}`, values)
 }
 
 export const remove = async (id) => {
-  await rollBackAccount(id)
+  await rollbackAccount(id)
   return Common.remove(`${BILL_URL}/${id}`)
 }
 
@@ -64,40 +64,66 @@ const updateTransferAccount = async (values) => {
   console.log(values)
 }
 
-const rollBackAccount = async (bill_id) => {
-  const { data: { type, account_id, money } } = await fetchById(bill_id)
-  const { data: { debt, balance } } = await Accounts.fetchById(account_id)
+const rollbackAccount = async (bill_id) => {
+  const { data: bill } = await fetchById(bill_id)
   // Rollback account balance
-  let newBalance = balance
-  switch (type) {
+  switch (bill.type) {
     case 'PAYMENT':
-      newBalance = debt ? (balance - money) : (balance + money)
+      await rollbackPaymentAccount(bill)
       break
     case 'INCOME':
-      newBalance = balance - money
+      await rollbackIncomeAccount(bill)
       break
     case 'TRANSFER':
-      // await rollBackTransferAccount(values)
+      await rollbackTransferAccount(bill)
       // 转账rollBack也需分解为两个操作 分别为回滚支出账户也就是PAYMENT；回滚转入账户也就是INCOME；
       //  需要做的就是再次重构账户rollback函数
       break
     default:
       break
   }
+}
+
+const rollbackPaymentAccount = async (bill) => {
+  const { account_id, money } = bill
+  const { data: { debt, balance } } = await Accounts.fetchById(account_id)
 
   await Accounts.patch({
     id: account_id,
     values: {
-      balance: newBalance
+      balance: debt ? (balance - money) : (balance + money)
     }
   })
 }
 
-const rollBackAndUpdateAccount = async ({ id: bill_id, values }) => {
+const rollbackIncomeAccount = async (bill) => {
+  const { account_id, money } = bill
+  const { data: { balance } } = await Accounts.fetchById(account_id)
+
+  await Accounts.patch({
+    id: account_id,
+    values: {
+      balance: (balance - money)
+    }
+  })
+}
+
+const rollbackTransferAccount = async (bill) => {
+  const {
+    payment_account_id,
+    income_account_id,
+    money
+  } = bill
+
+  rollbackIncomeAccount({ account_id: income_account_id, money })
+  rollbackPaymentAccount({ account_id: payment_account_id, money })
+}
+
+const rollbackAndUpdateAccount = async ({ id: bill_id, values }) => {
   const { type } = values
 
   // Rollback account balance
-  await rollBackAccount(bill_id)
+  await rollbackAccount(bill_id)
 
   // Update Account Balance
   switch (type) {
